3db09a9f22f69bfc9996fc290a21d2f2cb99d321
Havockeyan <karthihavoc121@gmail.com>
2025-07-06 08:19:55 +0530

    Fix for getting the properties file inside the jar for other environments

--- src/main/java/EmailSender.java
+++ src/main/java/EmailSender.java
@@ -8,13 +8,17 @@ import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 
 import java.util.Properties;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class EmailSender {
     public static void main(String[] args) {
         Properties envProps = new Properties();
-        try (FileInputStream fis = new FileInputStream("src/main/resources/email.properties")) {
+        try (InputStream fis = EmailSender.class.getClassLoader().getResourceAsStream("email.properties")) {
+            if (fis == null) {
+                System.err.println("Failed to load email.properties file from classpath");
+                return;
+            }
             envProps.load(fis);
         } catch (IOException e) {
             System.err.println("Failed to load email.properties file");
